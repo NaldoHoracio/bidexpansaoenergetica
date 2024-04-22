e0e543dd83d3961f697b48dda1b46f6ec6646d98
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2024-04-22 14:11:14 -0300

    Update "envia_para_database.R"

--- R/envia_para_database.R
+++ R/envia_para_database.R
@@ -10,13 +10,10 @@ library(odbc)
 # Check the operating system
 if (Sys.info()["sysname"] == "Windows") 
 {
-  # For Windows, set encoding for writing names with accentuation
   Sys.setlocale("LC_CTYPE", "Portuguese_Brazil.UTF-8")  # Change to the appropriate locale
   
 } else if (Sys.info()["sysname"] == "Darwin" || Sys.info()["sysname"] == "Linux") 
-{  # macOS (iOS not directly supported in R)
-  # For macOS (Darwin), convert names with accentuation to names without accentuation
-  #Sys.setlocale(category="LC_CTYPE", locale="en_US.UTF-8")
+{  
   Sys.setlocale(category="LC_CTYPE", locale="pt_BR.UTF-8")
 } else 
 {
